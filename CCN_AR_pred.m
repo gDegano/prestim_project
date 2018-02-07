9f64c38fee77df6721ab87f4d490b9efadb47c17
SteffenBuergers <sbuergers@gmail.com>
2018-02-07 21:49:59 +0000

    SB - added comments to CCN_AR_pred, and optional input of model order in opts. Added presentation from EEG meeting.

--- CCN_AR_pred.m
+++ CCN_AR_pred.m
@@ -1,6 +1,18 @@
 function [ yy,tt] = CCN_AR_pred(data2interp,time2interp,opts )
-% ARIMA prediction
+% Autoregressive model prediction (AR)
+% 
+% Takes a time series vector as input (1xN) and computes an
+% autoregressive model specified in 'opts' forecasting 'time2interp'
+% datapoints
 %
+% INPUT : data2interp  = 1xN time vector 
+%         time2interp  = number of datapoints to interpolate
+%         opts.srate   = sampling freq
+%         opts.secPH   = length of the data in seconds (?)
+%         opts.order   = number of coefficients of the AR model (default=7)
+%
+% OUTPUT: yy           = time course of original data plus forecast
+%         tt           = timeline of yy
 %
 % ------
 % Jan,2018 Giulio Degano & Steffen Buergers
@@ -10,12 +22,22 @@ function [ yy,tt] = CCN_AR_pred(data2interp,time2interp,opts )
 % SXB1173@student.bham.ac.uk 
 
 
+%% defaults
+
+if ~isfield(opts, 'order')
+    p = 7;
+else
+    p = opts.order;
+end
+
+
+%% AR model
+
 % Period
 Ts=1/opts.srate;
 % PH
 numPeriods=round(opts.secPH*opts.srate);
 ys=data2interp;
-p=7;
 
 % To data obj
 data = iddata(ys',[],Ts);
@@ -28,7 +50,8 @@ yy = forecast(model,data,numPeriods);
 tt_pred=time2interp(end)+Ts:Ts:opts.secPH;
 yy_pred=yy.y';
 
-%% NOISE COMPUTATION...
+
+%% Noise computation 
 
 flag_noise=1;
 
@@ -38,6 +61,7 @@ if flag_noise==1
 else
     yy_pred_noise=yy_pred;
 end
+
 
 %% Concatenate 
 
